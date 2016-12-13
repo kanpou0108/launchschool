7b3effc5f6ffaeea377cf38402d91b02b7eafbcc
Junpei Ihara <seapig.dolphin2925@gmail.com>
2016-12-13 13:38:10 +0900

    lesson_3/medium_2 added

---
+++ lesson_3/medium_2/q01.rb
@@ -0,0 +1,28 @@
+# Hashes are commonly found to be more complex than the simple one we saw earlier:
+# 
+# ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10 }
+# It is often the case that each key points to another hash instead of a single value:
+# 
+# munsters = {
+#   "Herman" => { "age" => 32, "gender" => "male" },
+#   "Lily" => { "age" => 30, "gender" => "female" },
+#   "Grandpa" => { "age" => 402, "gender" => "male" },
+#   "Eddie" => { "age" => 10, "gender" => "male" }
+# }
+# Figure out the total age of just the male members of the family.
+
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" }
+}
+
+sum = 0
+munsters.each do |k, v|
+  if v["gender"] = "male"
+    sum += v["age"]
+  end
+end
+
+p sum
---
+++ lesson_3/medium_2/q04_ans.rb
@@ -0,0 +1,5 @@
+sentence = "Humpty Dumpty sat on a wall."
+
+words = sentence.split(/\W/)
+words.reverse!
+p backwards_sentence = words.join(' ') + '.'
