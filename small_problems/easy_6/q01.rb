71fbeee5cf72270e18dc7c415cf7c09a6b1f3ce7
Junpei Ihara <seapig.dolphin2925@gmail.com>
2016-12-18 16:43:19 +0900

    small_problems/easy_6 q1 modified

--- small_problems/easy_6/q01.rb
+++ small_problems/easy_6/q01.rb
@@ -1,11 +1,13 @@
-# Cute angles
-# 
-# Write a method that takes a floating point number that represents an angle between 0 and 360 degrees and returns a String that represents that angle in degrees, minutes, and seconds. You should use a degree symbol (˚) to represent degrees, a single quote (') to represent minutes, and a double quote (") to represent seconds. There are 60 minutes in a degree, and 60 seconds in a minute.
-# 
-# Examples:
+DEGREE = "\xC2\xB0"
+MINUTES_PER_DEGREE = 60
+SECONDS_PER_MINUTE = 60
+SECONDS_PER_DEGREE = MINUTES_PER_DEGREE * SECONDS_PER_MINUTE
 
 def dms(float_number)
-  # .....
+  total_seconds = float_number * SECONDS_PER_DEGREE
+  degrees, remaining_seconds = total_seconds.divmod(SECONDS_PER_DEGREE)
+  minutes, seconds = remaining_seconds.divmod(SECONDS_PER_MINUTE)
+  format(%(#{degrees}#{DEGREE}%02d'%02d"), minutes, seconds)
 end
 
 p dms(30) == %(30°00'00")
@@ -15,3 +17,15 @@ p dms(93.034773) == %(93°02'05")
 p dms(0) == %(0°00'00")
 p dms(360) == %(360°00'00") || dms(360) == %(0°00'00")
 
+puts dms(30)
+puts dms(76.73)
+puts dms(254.6)
+puts dms(93.034773)
+puts dms(0)
+puts dms(360)
+
+# Cute angles
+# 
+# Write a method that takes a floating point number that represents an angle between 0 and 360 degrees and returns a String that represents that angle in degrees, minutes, and seconds. You should use a degree symbol (˚) to represent degrees, a single quote (') to represent minutes, and a double quote (") to represent seconds. There are 60 minutes in a degree, and 60 seconds in a minute.
+# 
+# Examples:
