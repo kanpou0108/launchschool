b5657ea01683b6ccc7df06f4d7cbffcc13704f5a
Junpei Ihara <seapig.dolphin2925@gmail.com>
2016-12-21 16:25:15 +0900

    small_problems/easy_8 q01-q02 added

---
+++ small_problems/easy_8/q01.rb
@@ -0,0 +1,22 @@
+# Sum of Sums
+# 
+# Write a method that takes an Array of numbers and then returns the sum of the sums of each leading subsequence for that Array. You may assume that the Array always contains at least one number.
+# 
+# Examples:
+
+def sum_of_sums(arr)
+  result_arr = []
+  sum = 0
+  arr.each do |e|
+    sum += e
+    result_arr << sum
+  end
+  result_arr.inject(:+)
+end
+
+# p sum_of_sums([3, 5, 2])
+
+p sum_of_sums([3, 5, 2]) == (3) + (3 + 5) + (3 + 5 + 2) # -> (21)
+p sum_of_sums([1, 5, 7, 3]) == (1) + (1 + 5) + (1 + 5 + 7) + (1 + 5 + 7 + 3) # -> (36)
+p sum_of_sums([4]) == 4
+p sum_of_sums([1, 2, 3, 4, 5]) == 35
