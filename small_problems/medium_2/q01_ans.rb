51ec9811b0f6f49c7dd1c1ae1f4017dee8668dbd
Junpei Ihara <seapig.dolphin2925@gmail.com>
2016-12-29 21:01:00 +0900

    small_problems/medium_2 q01 added

---
+++ small_problems/medium_2/q01_ans.rb
@@ -0,0 +1,10 @@
+# text = File.read('sample_text.txt')
+text = File.read('pg84.txt')
+sentences = text.split(/\.|\?|!/)
+largest_sentence = sentences.max_by { |sentence| sentence.split.size }
+largest_sentence = largest_sentence.strip
+number_of_words = largest_sentence.split.size
+
+puts "#{largest_sentence}"
+puts "Containing #{number_of_words} words"
+
