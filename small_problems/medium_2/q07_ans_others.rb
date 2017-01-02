2e3502e2bd3323ee35c91d2d093be7b63bebff9a
Junpei Ihara <seapig.dolphin2925@gmail.com>
2017-01-02 21:36:56 +0900

    small_problems/medium_2 q06-q07 added

---
+++ small_problems/medium_2/q07_ans_others.rb
@@ -0,0 +1,9 @@
+def friday_13th?(year)
+  friday_13ths = 0
+  1.upto(12) { |month| friday_13ths += 1 if Time.new(year, month, 13).friday? }
+  friday_13ths
+end
+
+p friday_13th?(2015) == 3
+p friday_13th?(1986) == 1
+p friday_13th?(2019) == 2
