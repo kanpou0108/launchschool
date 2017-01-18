7d24d9b56887d5da8a3cb72a0e45589c6759c1a8
Junpei Ihara <seapig.dolphin2925@gmail.com>
2017-01-18 17:17:50 +0900

    object_oriented_programming/oo_basics_01 q03 added

---
+++ object_oriented_programming/oo_basics_01/q03.rb
@@ -0,0 +1,15 @@
+# Create the Object
+# 
+# Using the code from the previous exercise, create an instance of Cat and assign it to a variable named kitty.
+# 
+# Code:
+# 
+class Cat
+end
+
+kitty = Cat.new
+
+# Discussion
+# To instantiate a new Cat object, we use the class method ::new. This method returns a new instance of the class it was invoked upon. We now have a Cat object.
+# 
+# To make use of this new Cat object, we need to assign it to a variable. In the solution, we assign the object to a variable named kitty. This variable can be used to interact with the object in various ways. We'll expand on this topic in the next few exercises.
